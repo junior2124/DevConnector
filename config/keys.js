ac9cfde2628e26cb4b116e77fff19e987c0480aa
Escobedo <emilio.escobedo@pciaa.net>
2018-10-26 15:06:15 -0500

    In Progress.

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://junior2124:junior2124@ds141633.mlab.com:41633/devconnectors"
+    "mongodb://junior2124:junior2124@ds141633.mlab.com:41633/devconnectors",
+  secretOrKey: "secret" // anything
 };
