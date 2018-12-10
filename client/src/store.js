0cd85c7f2fdb6abadb093a335d36458492922a97
Escobedo <emilio.escobedo@pciaa.net>
2018-12-10 14:43:38 -0600

    commit

--- client/src/store.js
+++ client/src/store.js
@@ -10,8 +10,11 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware)
-    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware),
+    window.navigator.userAgent.includes("Chrome")
+      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      : compose
   )
 );
 
