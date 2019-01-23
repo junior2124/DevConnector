53e58f4b87acc1aa2d54b686335fc67fe304c3a0
Escobedo <emilio.escobedo@pciaa.net>
2019-01-23 15:26:36 -0600

    New release

--- models/User.js
+++ models/User.js
@@ -25,25 +25,4 @@ const UserSchema = new Schema({
   }
 });
 
-UserSchema.pre("findOneAndUpdate", function(next) {
-  var user = this;
-  var SALT_WORK_FACTOR = 10;
-  // only hash the password if it has been modified (or is new)
-  if (!user.isModified("password")) return next();
-
-  // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if (err) return next(err);
-
-    // hash the password using our new salt
-    bcrypt.hash(user.password, salt, function(err, hash) {
-      if (err) return next(err);
-
-      // override the cleartext password with the hashed one
-      user.password = hash;
-      next();
-    });
-  });
-});
-
 module.exports = User = mongoose.model("users", UserSchema);
